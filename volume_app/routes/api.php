1ff7eff41d207fb7c7a140b9a059c3ba95e79cdd
Bruno Almeida <brunoritchieledev@gmail.com>
2025-07-16 14:13:22 -0300

    correção de bug

--- volume_app/routes/api.php
+++ volume_app/routes/api.php
@@ -10,16 +10,7 @@ Route::get('/status', function () {
     return ApiResponse::succes('API is running');
 })->middleware('auth:sanctum');
 
-Route::get('clients', [ClientController::class, 'index'])->middleware('auth:sanctum');
-Route::post('clients', [ClientController::class, 'store'])->middleware('auth:sanctum');
-Route::get('clients/{client}', [ClientController::class, 'show'])->middleware('auth:sanctum');
-Route::put('clients/{client}', [ClientController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('clients/{client}', [ClientController::class, 'destroy'])->middleware('auth:sanctum');
-
-
-
 //auth routes
-Route::apiResource('Clients', ClientController::class)->middleware('auth:sanctum');
+Route::apiResource('clients', ClientController::class)->middleware('auth:sanctum');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
-    
