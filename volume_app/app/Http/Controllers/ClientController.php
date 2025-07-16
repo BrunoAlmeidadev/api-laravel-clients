4f1a77453f8ccc379786fb14fa2c207d49ea6873
Bruno Almeida <brunoritchieledev@gmail.com>
2025-07-16 00:20:07 -0300

    006

---
+++ volume_app/app/Http/Controllers/ClientController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Client::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|',
+            'email' => 'required|email|unique:clients,email',
+            'phone' => 'required|string'
+        ]);
+
+        $client = Client::create($request->all());
+
+        return response()->json(
+            [
+                'message' => 'Client created successfully',
+                'data' => $client
+
+            ],200
+        
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $client = Client::find($id);
+
+        if($client) {
+            return response()->json($client, 200);
+        
+        }else{
+            return response()->json(
+                [
+                    'message' => 'Client not found'
+                ], 404
+            );
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
